f4c7e150c97bdaea3cfaeac5eda02aa83d4c21b7
Thiago Zilli <thiago.zilli@gmail.com>
2016-02-25 17:08:09 -0300

    [NEW] first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let _ = require('underscore');
+let scrape = require('metatag-crawler');
+let rest = require('restling');
+let Promise = require('bluebird');
+let DownloadProgress = require('download-progress');
+
+function getfile(urls) {
+  return new Promise((resolve, reject) => {
+    let options = {};
+    let download = DownloadProgress(urls, options);
+    download.get((err) => {
+      if (err) {
+        reject(new Error(err));
+      }
+      resolve('DONE');
+    });
+  });
+}
+
+function parse(url) {
+  return new Promise((resolve, reject) => {
+    scrape(url, (err, data) => {
+      rest.get(data.og.videos[0].url).then(function(result) {
+        let myRegexp = /data-config=\"(.*?)\"/g;
+        let match = myRegexp.exec(result.data);
+        let json = match[1].replace(/\&quot;/g, '"');
+        let urlvid = JSON.parse(json).video_url;
+        resolve(urlvid);
+      }, function(error) {
+        if (error.response) {
+          reject(error.response);
+        }
+      });
+    });
+  });
+}
+
+function download(url) {
+  return new Promise((resolve, reject) => {
+    parse(url).then((result) => {
+      let urls = [{
+        url: result,
+        dest: 'downloads/' + result.split('/').pop()
+      }];
+      getfile(urls).then((result) => {
+        resolve(result);
+      }).catch((err) => {
+        reject(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
+}
+
+//let file = 'https://twitter.com/morenatoppp/status/702325281906925570';
+let file = 'https://twitter.com/morenatoppp/status/691570549919748096';
+
+download(file).then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.log(err);
+});
